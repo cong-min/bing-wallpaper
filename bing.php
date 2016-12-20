898171a4d5783938353d48b2ef4c00b8420365ef
Cong Min <i@congm.in>
2016-12-20 19:53:44 +0800

    更新接口

--- bing.php
+++ bing.php
@@ -1,17 +1,18 @@
 <?php
 	$str = file_get_contents('http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
 	$data = json_decode($str);
-	$imgurl = $data -> {"images"}[0] -> {"url"};
-	if($imgurl){
+	$imghost = 'http://cn.bing.com';
+	$imgpath = $data -> {"images"}[0] -> {"url"};
+	if($imgpath){
+	  $imgurl = $imghost . $imgpath;
 		$img = imagecreatefromjpeg($imgurl);
-		header('Expires: ' . gmdate('D, d M Y H:i:s', strtotime(date('Y-m-d', strtotime('+1 day')))) . ' GMT'); 
+		header('Expires: ' . gmdate('D, d M Y H:i:s', strtotime(date('Y-m-d', strtotime('+1 day')))) . ' GMT');
 		header('Cache-Control: public, max-age=3600');
-		header('Last-Modified: ' . gmdate('D, d M Y H:i:s', strtotime(date('Y-m-d'))) . ' GMT'); 
+		header('Last-Modified: ' . gmdate('D, d M Y H:i:s', strtotime(date('Y-m-d'))) . ' GMT');
 		header('Content-Type: image/jpeg');
 		imageinterlace($img, 1);
 		imagejpeg($img);
 		imagedestroy($img);
-		exit();
 	}else{
 		exit('error');
 	}
