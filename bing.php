3e7aaed14b0e98b093142db988fe585a95e9a57f
Cong Min <i@congm.in>
2017-07-07 16:54:59 +0800

    http -> https

--- bing.php
+++ bing.php
@@ -1,7 +1,7 @@
 <?php
 	$str = file_get_contents('http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
 	$data = json_decode($str);
-	$imghost = 'http://cn.bing.com';
+	$imghost = 'https://cn.bing.com';
 	$imgpath = $data -> {"images"}[0] -> {"url"};
 	if($imgpath){
 	  $imgurl = $imghost . $imgpath;
