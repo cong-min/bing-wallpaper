b29fdb858794204fee270a03c83febf7444c59bf
Cong Min <i@congm.in>
2025-10-19 11:30:55 +0800

    Add cloudflare worker version

---
+++ bing_cloudflare_worker.js
@@ -0,0 +1,11 @@
+addEventListener('fetch', event => {
+    event.respondWith((async () => {
+        try {
+            const data = await fetch('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US').then(r => r.json());
+            const imgpath = data.images[0].url;
+            return imgpath ? Response.redirect('https://www.bing.com' + imgpath, 302) : new Response('error', { status: 500 });
+        } catch {
+            return new Response('error', { status: 500 });
+        }
+    })());
+});
